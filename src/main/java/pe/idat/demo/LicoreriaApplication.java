9846772986800dc17c2310231c98e3e220d0ec79
Erickninguno01 <118143303+Erickninguno01@users.noreply.github.com>
2022-11-13 21:24:13 -0500

    Prueba1
    prueba1

---
+++ src/main/java/pe/idat/demo/LicoreriaApplication.java
@@ -0,0 +1,13 @@
+package pe.idat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicoreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicoreriaApplication.class, args);
+	}
+
+}
