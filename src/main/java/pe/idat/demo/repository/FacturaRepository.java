6f2a13efd93ab916bf389d50ffa3b14fa6b6ad2e
Erickninguno01 <118143303+Erickninguno01@users.noreply.github.com>
2022-11-15 18:37:39 -0500

    factura
    factura

---
+++ src/main/java/pe/idat/demo/repository/FacturaRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pe.idat.demo.repository;
+
+/**
+ *
+ * @author user
+ */
+public interface FacturaRepository {
+    
+}
