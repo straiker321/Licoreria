9b7f892ce2343cafc2581af33a0ae9cbca01d8a7
straiker321 <95499138+straiker321@users.noreply.github.com>
2022-11-15 16:05:14 -0500

    proveedores

--- src/main/java/pe/idat/demo/entity/proveedores.java
+++ src/main/java/pe/idat/demo/entity/proveedores.java
@@ -4,10 +4,34 @@
  */
 package pe.idat.demo.entity;
 
-/**
- *
- * @author erick
- */
-public class proveedores {
-    
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "proveedores")
+@Table(name = "proveedores")
+public class proveedores implements Serializable {
+    private static final long serialVersionUID=1L;
+    @Id
+    @Column(name = "id_proveedores")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_proveedores;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "direccion")
+    private String direccion;
+    @Column(name = "telefono")
+    private String telefono;
 }
