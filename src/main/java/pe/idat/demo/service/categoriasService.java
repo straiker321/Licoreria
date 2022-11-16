93c5618b4d96907678902aa74c01b19ce45d6980
JosueML24 <76792913+JosueML24@users.noreply.github.com>
2022-11-16 18:27:44 -0500

    sub

--- src/main/java/pe/idat/demo/service/categoriasService.java
+++ src/main/java/pe/idat/demo/service/categoriasService.java
@@ -1,15 +1,19 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
 package pe.idat.demo.service;
 
 import java.util.List;
 import java.util.Optional;
-
 import pe.idat.demo.entity.categorias;
 
-
+/**
+ *
+ * @author Harold Mallma
+ */
 public interface categoriasService {
-
-    public List<categorias> findAll();
+     public List<categorias> findAll();
     
     public List<categorias> findAllCustom();
     
